2bc92dfc3db47655fc14e32538d1185cf4a06cbb
Shawn-source-debug <140548136+Shawn-source-debug@users.noreply.github.com>
2026-01-12 20:41:26 -0500

    Remove Ottawa location from contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -56,13 +56,6 @@ function Contact() {
           <div className="contact-info">
             <div className="info-item">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                <circle cx="12" cy="10" r="3"></circle>
-              </svg>
-              <span>Ottawa, Ontario, Canada</span>
-            </div>
-            <div className="info-item">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
                 <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
               </svg>
